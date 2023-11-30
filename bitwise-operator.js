cc0e78385d122c33008597a7aaa7d7e17c0d62ad
Bharat <itsd23ree6ix.com>
2023-11-30 08:31:22 +0530

    ad comenting

--- bitwise-operator.js
+++ bitwise-operator.js
@@ -20,9 +20,9 @@ function bitwiseRightShift(num, num2) {
 let num1 = 5; // 0101
 let num2 = 2; // 0010
 
-console.log(`${num1} & ${num2}= ${bitwiseAND(num1, num2)}`);
-console.log(`${num1} | ${num2}= ${bitwiseOR(num1, num2)}`);
-console.log(`${num1} ~ ${num2}= ${bitwiseNOT(num1, num2)}`);
-console.log(`${num1} ^ ${num2}= ${bitwiseXOR(num1, num2)}`);
-console.log(`${num1} << ${num2}= ${bitwiseLeftShift(num1, num2)}`);
-console.log(`${num1} >> ${num2}= ${bitwiseRightShift(num1, num2)}`);
+console.log(`${num1} & ${num2}= ${bitwiseAND(num1, num2)}`); // 0000 = 0
+console.log(`${num1} | ${num2}= ${bitwiseOR(num1, num2)}`); // 0111 = 7
+console.log(`${num1} = ${bitwiseNOT(num1)}`); // 1010 = -6
+console.log(`${num1} ^ ${num2}= ${bitwiseXOR(num1, num2)}`); // 0111 = 7
+console.log(`${num1} << ${num2}= ${bitwiseLeftShift(num1, num2)}`); // 010100 = 20
+console.log(`${num1} >> ${num2}= ${bitwiseRightShift(num1, num2)}`); // 000001 = 1
